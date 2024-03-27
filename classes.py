2efad994e957dd3c9e76f09781bb22e00b190855
Nikk Forbus <27784291+nforbus@users.noreply.github.com>
2024-03-27 13:56:37 -0700

    Updated classes.py to include getters/setters, hidden attributes.

--- classes.py
+++ classes.py
@@ -1,11 +1,40 @@
 
 class Car:
-    def __init__(self, make="unknown", model="unknown", color="unknown", year=-1, price=-1):
+    def __init__(self, make="unknown", model="unknown", color="unknown", _year=-1, _price=-1):
         self.make = make
         self.model = model
         self.color = color
-        self.year = year
-        self.price = price
+        self._year = _year
+        self._price = _price
 
-car = Car("buick","lesabre","red",2013,10000)        
+
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, price_to_add):
+        if(price_to_add <= 0):
+            raise ValueError("Price is zero or less.")
+        print("Setter for price called, value is: " + str(price_to_add))
+        self._price = price_to_add
+
+    @property
+    def year(self):
+        return self._year
+    
+    @year.setter
+    def year(self, year_to_add):
+        if(year_to_add <= 1799):
+            raise ValueError("Cars didn't exist back then.")
+        print("Setter for year called, value is: " + self._year)
+        self._year = year_to_add
+
+
+car = Car()
 print("Model = " + car.model)
+car.price = 10000
+print("price is: " + str(car.price))
+
+car.year = 1750
+print("year is: " + str(car.year))
