c924e3e561cbfd8031a96f60a43507030898855d
Nikk Forbus <27784291+nforbus@users.noreply.github.com>
2024-03-28 10:23:28 -0700

    Completed course, added file-handler support, object sorting, strig representation.  Added jupyter notebook (completed).

--- classes.py
+++ classes.py
@@ -27,14 +27,25 @@ class Car:
     def year(self, year_to_add):
         if(year_to_add <= 1799):
             raise ValueError("Cars didn't exist back then.")
-        print("Setter for year called, value is: " + self._year)
+        print("Setter for year called, value is: " + str(self._year))
         self._year = year_to_add
 
+    def __str__(self):
+        return 'Car(make = ' + str(self.make) + ', model = ' + str(self.model) +\
+            ', color = ' + str(self.color) + ', year = ' + str(self.year) +\
+            ', price = ' + str(self.price) + ')'
+    
 
-car = Car()
-print("Model = " + car.model)
-car.price = 10000
-print("price is: " + str(car.price))
+file_handler = open("cars.csv", "r")
+cars_data = file_handler.readlines()
+cars_data.pop(0) #removes the first entry, which in this case is the column headers we don't want.
 
-car.year = 1750
-print("year is: " + str(car.year))
+cars_list = []
+
+for raw_string in cars_data:
+    make, model, color, year, price = raw_string.split(',')
+    cars_list.append(Car(make, model, color, int(year), float(price)))
+
+cars_list.sort(key=lambda car: car.price)
+
+print(*cars_list, sep="\n")
